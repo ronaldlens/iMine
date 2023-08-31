b40003d23fd83ef0c1dc7b60a6af6a63a1a2c023
Ronald Lens <ronald@ronaldlens.com>
2023-08-31 18:11:14 +0200

    Initial Commit

---
+++ iMine/iMineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMineApp.swift
+//  iMine
+//
+//  Created by Ronald Lens on 31/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct iMineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
